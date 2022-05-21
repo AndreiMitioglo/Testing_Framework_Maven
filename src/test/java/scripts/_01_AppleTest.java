9f71f7451803ed0193d02628f3e937d8a9415e44
AndreiMitioglo <mitioandrew2@gmail.com>
2022-05-21 16:08:59 -0500

    added class

---
+++ src/test/java/scripts/_01_AppleTest.java
@@ -0,0 +1,60 @@
+package scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class _01_AppleTest {
+    //Declare your driver
+    WebDriver driver;
+
+    // 1. Initial set up - before each test
+    @BeforeMethod
+    public void setup(){
+        driver = Driver.getDriver();
+    }
+
+    // 2. Validation of test - test
+    @Test
+    public void testAppleTitle(){
+        driver.get("https://www.apple.com/");
+
+        String expectedTitle = "Apple"; // requirement
+        String actualTitle = driver.getTitle(); // development
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+
+       // if(actualTitle.equals(expectedTitle)) System.out.println("Apple title validation passed");
+       // else System.out.println("Apple title validation failed");
+    }
+
+
+
+    @Test
+    public void testAppleUrl(){
+        driver.get("https://www.apple.com/");
+        String expectedUrl = "https://www.apple.com/"; // requirement
+        String actualUrl = driver.getCurrentUrl(); // development
+
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+    }
+
+    @Test
+    public void X(){
+        //fail() method is used to fail a test on purpose
+        // we use this for known issues
+        Assert.fail("Failure on purpose");
+    }
+
+     @AfterMethod
+    // 3. Teardown actions - after each test
+    public void teardown(){
+        Driver.quitDriver();
+    }
+
+    }
